0fc33d20ecdefeded557d7a9bd748a2fd7ad3a17
Ashok <k.ashokachary@gmail.com>
2020-12-20 08:51:17 +0530

    change on location

---
+++ src/com/python/testFunctions.py
@@ -0,0 +1,31 @@
+'''
+Created on 20-Dec-2020
+
+    Learning of function of python
+
+@author: kasho
+'''
+
+def fib(n):
+    """Print a Fibonacci series up to n."""
+    a, b = 0, 1
+    while a < n:
+        print(a, end=' ')
+        a, b = b, a + b
+    print()
+
+def ask_ok(prompt, retries=4, reminder='Please try again!'):
+    while True:
+        ok = input(prompt)
+        if ok in ('y', 'ye', 'yes'):
+            return True
+        if ok in ('n', 'no', 'nop', 'nope'):
+            return False
+        retries = retries - 1
+        if retries < 0:
+            raise ValueError('invalid user response')
+        print(reminder)
+
+if __name__ == '__main__':
+    fib(2000)
+    ask_ok('OK to overwrite the file?')
